e19ef912a9e03b6214cc949b8f953eb278146d15
inzahanyang <inzahanyang@gmail.com>
2023-04-17 05:27:27 +0900

    signup

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
   const router = useRouter();
   useEffect(() => {
     if (userData && !userData.ok) {
-      router.push("/login");
+      router.push("/signup");
     }
   }, [userData, router]);
 
